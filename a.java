9e1a86ab511ee0b03525c14a6b38558ee10691a7
Administrator <1209607051@qq.com>
2020-01-02 14:47:04 +0800

    韩庆超

--- a.java
+++ a.java
@@ -3,7 +3,9 @@ public class a{
    int a = 5 ;
    int b = 10;
    int sum = a + b;
-   System.out.println("hanqingchao")
+   System.out.println("hanqingchao");
+   System.out.println("2");
+
    System.out.println(sum);
   }
 }
